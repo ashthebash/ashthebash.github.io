a3717b4fecf2b53e220ea82f7985b184110e27fd
ashthebash <7148133+ashthebash@users.noreply.github.com>
2021-09-26 15:00:06 +0100

    Update app.js to make my face spin.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src="https://i.imgur.com/96XuhDI.jpeg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>I'm Working on it!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
